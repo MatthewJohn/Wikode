c960c614c52e4917c531103ea48573ce4ffdf65e
Matt Comben <matthew@dockstudios.co.uk>
2020-09-25 07:05:24 +0100

    Add config loading from config JSON file

--- wikode/config.py
+++ wikode/config.py
@@ -1,9 +1,14 @@
 
 
 from enum import Enum
+import json
+import os
 
 
 class Config(object):
+
+    CONFIG_PATH = './config.json'
+    CONFIG_CACHE = None
 
     class KEYS(Enum):
         SCM_TYPE = 'SCM_TYPE'
@@ -17,10 +22,29 @@ class Config(object):
     }
 
     @staticmethod
+    def load_config():
+        if os.path.isfile(Config.CONFIG_PATH):
+            try:
+                with open(Config.CONFIG_PATH, 'r') as fh:
+                    return json.loads(fh.read())
+            except Exception as exc:
+                print('WARNING: Unable to load config: {0}'.format(str(exc)))
+        else:
+            print('INFO: config.json does not exist - using defaults')
+        return {}
+
+    @staticmethod
+    def get_config():
+        if Config.CONFIG_CACHE is None:
+            Config.CONFIG_CACHE = Config.load_config()
+        return Config.CONFIG_CACHE
+
+    @staticmethod
     def get(config_name):
+        if config_name.value in Config.get_config():
+            return Config.get_config()[config_name.value]
         return Config.DEFAULTS[config_name]
 
     @staticmethod
     def get_default(config_name):
         return Config.DEFAULTS[config_name]
-
