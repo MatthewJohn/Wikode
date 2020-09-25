2b619e2cfd6b11207badfb0b3e276d9e17a69eb4
Matt Comben <matthew@dockstudios.co.uk>
2020-09-25 19:45:50 +0100

    Add basic working search Issue #10

--- wikode/indexer.py
+++ wikode/indexer.py
@@ -1,6 +1,8 @@
 
 import sqlite3
 import glob
+import os
+
 from flask import request, render_template
 
 from wikode.config import Config
@@ -20,6 +22,10 @@ class DatabaseFactory(object):
         return cls.CONNECTION
 
     @classmethod
+    def sql_connect(cls):
+        return sqlite3.connect(Config.get(Config.KEYS.SQLITE_PATH))
+
+    @classmethod
     def get_cursor(cls):
         """Return database cursor."""
         return cls.get_connection().cursor()
@@ -27,39 +33,48 @@ class DatabaseFactory(object):
 
 class Indexer(object):
 
-    TABLE_NAME = 'wikis'
+    TABLE_NAME = 'wiki'
 
     def initialise_database(self):
-        c = DatabaseFactory.get_cursor().execute(
-            """
-            SELECT name FROM sqlite_master WHERE type='table' AND name=?;
-            """,
-            (Indexer.TABLE_NAME,)
-        )
-        if not c.rowcount:
-            DatabaseFactory.get_cursor().execute("""
-                CREATE VIRTUAL TABLE wiki
-                USING FTS5(url, file, content);
-            """)
+        with DatabaseFactory.sql_connect() as db:
+            c = db.cursor()
+            r = c.execute(
+                """
+                SELECT name FROM sqlite_master WHERE type='table' AND name=?;
+                """,
+                (Indexer.TABLE_NAME,)
+            )
+            if not len(r.fetchall()):
+                c.execute("""
+                    CREATE VIRTUAL TABLE wiki
+                    USING FTS5(url, file, content);
+                """)
+                self.reindex_all_files()
 
     def search(self, search_string):
-        r = DatabaseFactory.get_cursor().execute(
-            """SELECT url FROM wiki WHERE wiki MATCH ?""",
-            (search_string,))
-        return [i['url'] for i in r]
+        with DatabaseFactory.sql_connect() as db:
+            c = db.cursor()
+            r = c.execute(
+                """SELECT url FROM wiki WHERE wiki MATCH ?""",
+                (search_string,)).fetchall()
+            print(r)
+            return [i[0] for i in r]
 
     def index_file(self, wiki):
-        c = DatabaseFactory.get_cursor()
-        c.execute("""DELETE FROM wiki WHERE file= ?""", (wiki.file_path,))
-        c.execute(
-            """INSERT INTO wiki(file, url, content) VALUES(?, ?, ?)""",
-            (wiki.file_path, wiki.url_struct, wiki.rendered))
-        c.commit()
+        print('Indexing file: {0}'.format(wiki.url_struct))
+        with DatabaseFactory.sql_connect() as db:
+            c = db.cursor()
+            c.execute("""DELETE FROM wiki WHERE file= ?""", (wiki.file_path,))
+            c.execute(
+                """INSERT INTO wiki(file, url, content) VALUES(?, ?, ?)""",
+                (wiki.file_path, wiki.url_struct, wiki.rendered))
+            db.commit()
 
     def reindex_all_files(self):
-        for f in glob.glob(
-                Config.get(Config.KEYS.DATA_DIR),
-                '**/*' + Wiki.FILE_EXTENSION, recursive=True):
+        for f in glob.glob(os.path.join(
+                    Config.get(Config.KEYS.DATA_DIR),
+                    '**/*' + Wiki.FILE_EXTENSION),
+                recursive=True):
             wiki = Wiki(Wiki.filename_to_url(f))
             self.index_file(wiki)
 
