0bbae5218a71ffe512304c926ed2c72ca4f2b384
Matt Comben <matthew@dockstudios.co.uk>
2020-09-25 17:20:33 +0100

    Start work on indeder/database Issue #10

---
+++ wikode/indexer.py
@@ -0,0 +1,24 @@
+
+import sqlite3
+
+from wikode.config import Config
+
+
+class DatabaseFactory(object):
+    """Provide interfaces to obtain connections to database."""
+
+    CONNECTION = None
+
+    @classmethod
+    def get_connection(cls):
+        """Create and return singleton connection to sqlite database."""
+        if cls.CONNECTION is None:
+            cls.CONNECTION = sqlite3.connect(Config.get(Config.KEYS.SQLITE_PATH))
+        return cls.CONNECTION
+
+    @classmethod
+    def get_curosor(cls):
+        """Return database cursor."""
+        return cls.get_connection().cursor()
+
+
