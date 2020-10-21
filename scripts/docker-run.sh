db1a127bc1e51d105863917397ca96e588ef86f1
Matt Comben <matt.comben@jobsite.co.uk>
2020-10-21 08:30:01 +0100

    Update docker-run command to copy .ssh directory, for use with git and SVN

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-docker run -p 5000:5000 -v `pwd`/data:/app/data -v `pwd`/config.json:/app/config.json -v `pwd`/db.sqlite:/app/db.sqlite wikode:latest
+docker run -p 5000:5000 -v $HOME/.ssh:/root/.ssh:ro -v `pwd`/data:/app/data -v `pwd`/config.json:/app/config.json -v `pwd`/db.sqlite:/app/db.sqlite wikode:latest
 
